280f714d8952c0363a1d61454e290bf636ba25c8
Austin73 <maneeshpal85@gmail.com>
2022-11-27 10:33:11 +0530

    database designa and generate schema

---
+++ Bank Of People.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "accounts" (
+  "id" bigserial PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" bigserial PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestampz DEFAULT (now())
+);
+
+CREATE TABLE "transers" (
+  "id" bigserial PRIMARY KEY,
+  "from_account_id" bigint,
+  "to_account_id" bigint,
+  "amount" bigint NOT NULL,
+  "created_at" timestampz DEFAULT (now())
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transers" ("from_account_id");
+
+CREATE INDEX ON "transers" ("to_account_id");
+
+CREATE INDEX ON "transers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or postive';
+
+COMMENT ON COLUMN "transers"."amount" IS 'amount should be postive';
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
